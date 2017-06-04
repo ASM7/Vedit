9d5ae5d7205e18c38c2a4369898ea994099d3ee6
AZ404 <ztbzak1@rambler.ru>
2017-06-04 21:52:46 +0500

    basic shapes

---
+++ Vedit/Domain/Segment.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+using Vedit.Infrastructure;
+
+namespace Vedit.Domain
+{
+    public class Segment: IShape
+    {
+        public Vector Position { get; set; }
+        public Size BoundingRectSize { get; set; }
+        public double Angle { get; set; }
+        public float LineWidth { get; set; }
+        public Color Color { get; set; }
+
+        public void Paint(ICanvas canvas)
+        {
+            var pen = new Pen(Color, LineWidth);
+            using (var graphics = canvas.StartDrawing())
+            {
+                graphics.DrawLine(
+                    pen,
+                    Position.ToDrawingPoint(),
+                    (Position + new Vector(0, BoundingRectSize.Width)).ToDrawingPoint());
+            }
+        }
+    }
+}
