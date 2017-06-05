2a22ec672a289da0c418d98c5bcf108885687e65
AZ404 <ztbzak1@rambler.ru>
2017-06-05 11:15:02 +0500

    Document + Formatter

---
+++ Vedit/Domain/BinDocumentFormatter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using Vedit.Infrastructure;
+
+namespace Vedit.Domain
+{
+    public class BinDocumentFormatter : IFormatter<Document>
+    {
+        private readonly BinaryFormatter formatter;
+
+        public BinDocumentFormatter(BinaryFormatter formatter)
+        {
+            this.formatter = formatter;
+        }
+
+        public IEnumerable<byte> Serialize(Document obj)
+        {
+            var stream = new MemoryStream();
+            formatter.Serialize(stream, obj);
+            return stream.GetBuffer();
+        }
+
+        public Document Deserialize(IEnumerable<byte> bytes)
+        {
+            return (Document) formatter.Deserialize(new MemoryStream(bytes.ToArray()));
+        }
+    }
+}
