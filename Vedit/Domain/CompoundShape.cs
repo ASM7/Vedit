1716f6c09b58df87d51d30041c5b5cbc155a6178
AZ404 <ztbzak1@rambler.ru>
2017-06-05 11:00:32 +0500

    prototype

---
+++ Vedit/Domain/CompoundShape.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using Vedit.Infrastructure;
+
+namespace Vedit.Domain
+{
+    // TODO proportional resize
+    public abstract class CompoundShape : IShape
+    {
+        public Vector Position { get; set; }
+        public Size BoundingRectSize { get; set; }
+        public double Angle { get; set; }
+
+        protected IShape[] elements = {};
+
+        public void Paint(ICanvas canvas)
+        {
+            foreach (var element in elements)
+            {
+                element.Paint(canvas);
+            }
+        }
+    }
+}
---
+++ Vedit/Domain/Rectangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Vedit.Infrastructure;
+
+namespace Vedit.Domain
+{
+    public class Rectangle : CompoundShape
+    {
+        public Rectangle()
+        {
+            var segmentSize = new Size(1, 0);
+            var angle90 = Math.PI/2;
+            elements = new[]
+            {
+                new Segment() {Position = new Vector(0, 0), BoundingRectSize = segmentSize, Angle = 0},
+                new Segment() {Position = new Vector(1, 0), BoundingRectSize = segmentSize, Angle = angle90},
+                new Segment() {Position = new Vector(1, 1), BoundingRectSize = segmentSize, Angle = angle90*2},
+                new Segment() {Position = new Vector(0, 1), BoundingRectSize = segmentSize, Angle = angle90*3}
+            };
+        }
+    }
+}
