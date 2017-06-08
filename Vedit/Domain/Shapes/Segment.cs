58775a406d904c1f9490f88d23ea21bac134a8ab
AZ404 <ztbzak1@rambler.ru>
2017-06-08 12:31:57 +0500

    drawing Segment in center of BoundingRect

--- Vedit/Domain/Shapes/Segment.cs
+++ Vedit/Domain/Shapes/Segment.cs
@@ -7,13 +7,13 @@ namespace Vedit.Domain.Shapes
     [Serializable]
     public class Segment: DrawableObject, IShape
     {
-        public float LineWidth { get; set; } = 10;
+        public float LineWidth { get; set; } = 3;
         public Color Color { get; set; } = Color.Black;
         
         protected override void PaintStraight(Graphics graphics)
         {
             var pen = new Pen(Color, LineWidth);
-            graphics.DrawLine(pen, 0, 0, BoundingRectSize.Width, 0);
+            graphics.DrawLine(pen, 0, BoundingRectSize.Height / 2, BoundingRectSize.Width, BoundingRectSize.Height / 2);
         }
     }
 }
